df3c83345adee578342d4c366e3562105514142b
pruzanov <pruzanov@oicr.on.ca>
2025-09-23 21:34:34 -0400

    Some simplifications

--- runConfigScanner.py
+++ runConfigScanner.py
@@ -2,7 +2,10 @@
    Config scanner uses some ideas (and code) from workflowTracker, but to a small extent
    and modified (simplified) for local file parsing. It uses .toml configuration file to
    point the scripts to the directories with olives (.shesmu instruction files) and configuration
-   file for assays (assay_info.jsonconfig)
+   file for assays (assay_info.jsonconfig). It may also take a path to a version control file
+   which tracks the versions of workflows used by various assays. A special freeze flag allows
+   to prevent updates to existing records, making it possible to run multiple versions of the same
+   workflow in parallel, for specific assays
 
    By parsing both olives and assay config file we can get information on
    a) which flags the scanned olives are using to run workflows (and which version)
@@ -34,7 +37,7 @@ def load_settings(path):
 
 
 """
-   Load assay/project setting according to the config, we need only enabled workflows
+   Load assay setting according to the config, we need only enabled workflows
 """
 def load_config(path):
     json_data = {}
@@ -50,26 +53,11 @@ def load_config(path):
     return json_data
 
 
-"""
-   A small utility function for vetting version list (depends on settings)
-"""
-def get_vetted_versions(oli_name: str, oli_tags, tracked_versions):
-    reported_olives = list(oli_tags)[0] if len(oli_tags) == 1 else list(oli_tags)
-    try:
-        if 'freeze' in tracked_versions.keys():
-            reported_olives = tracked_versions['workflows'][oli_name]
-        else:
-            reported_olives = list(set(reported_olives, tracked_versions['workflows'][oli_name]))
-    finally:
-        return reported_olives
-
-
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Run parsing script to generate assay scan report table')
     parser.add_argument('-s', '--settings', help='Settings file in TOML format', required=False, default="config.toml")
     parser.add_argument('-i', '--instance', help='Instance to scan', required=True)
     parser.add_argument('-o', '--out-json', help='Output json', required=False, default="enabled_workflows.json")
-    parser.add_argument('-c', '--check', help='Check type', required=False, default="assay")
     parser.add_argument('-j', '--jscript', help="Pass to HTML UI javascript", required=True)
     parser.add_argument('-r', '--versions', help="Pass an optional json file with version info", required=False)
     parser.add_argument('-p', '--outpage', help='Output page, HTML', required=False, default="running_workflows.html")
@@ -77,7 +65,6 @@ if __name__ == '__main__':
 
     settings_path = args.settings
     instance_to_scan = args.instance
-    check_type = args.check
     output_json = args.out_json
     output_page = args.outpage
     java_script = args.jscript
@@ -88,13 +75,16 @@ if __name__ == '__main__':
     if instance_to_scan not in settings["instances"].values():
         print(f"ERROR: instance {instance_to_scan} is not configured, aborting")
         exit(1)
+    ''' If we do not have version file, try to find it in the repo dir '''
+    if version_file is None and "version_file" in settings['data'].keys():
+        version_file = settings['data']['version_file']
     ''' 2. We have search patterns in config file, compile them here '''
     config_check = None
     try:
-        check_pattern = settings["checks"][check_type]
+        check_pattern = settings["checks"]["assay"]
         config_check = re.compile(f'({check_pattern})' + '(?P<check>\\S+)')
     except:
-        print(f"Failed to compile a search pattern for {check_type} type check")
+        print("Failed to compile a search pattern for olive check detection")
 
     ''' 3. collect and process olives, extract modules and tags '''
     olive_files = {}
@@ -106,16 +96,15 @@ if __name__ == '__main__':
     olive_files = gsiOlive.collect_olives(settings["data"]["local_olive_dir"], [instance_to_scan], blacklist, {})
     olive_info = gsiOlive.parse_olives(olive_files[instance_to_scan], config_check)
 
-    ''' 4. with loaded config file check the projects (or assays) for enabled workflows and construct the report '''
+    ''' 4. with loaded config file check the assays for enabled workflows and construct the report '''
     try:
-        config_data = load_config(settings["data"]["assay_config_file"]) if check_type == "assay" else \
-                      load_config(settings["data"]["project_config_file"])
+        config_data = load_config(settings["data"]["assay_config_file"])
     except:
         print("No config file configured in the settings")
         exit(1)
 
     '''Load and update the version settings, if available'''
-    confScanner = configScanner(config_data, olive_info, version_file)
+    confScanner = configScanner(config_data, olive_info, output_json, version_file)
     vetted_report = confScanner.get_report()
 
     '''Check that we have a non-optional javascript file'''
@@ -126,13 +115,11 @@ if __name__ == '__main__':
     ''' 5. Dump the data into json file and generate a report HTML page '''
     if len(vetted_report) > 0:
         confScanner.save_report(output_json)
-        # TODO: check why we do not have arrays for wf versions
-        '''HTML output is supported only for assay config, for project-level config only .json is generated'''
-        if check_type == "assay":
-            html_page = htmlRenderer.convert2page(output_json, java_script)
-            with open(output_page, 'w') as op:
-                op.write(html_page)
+        html_page = htmlRenderer.convert2page(output_json, java_script, instance_to_scan)
+        with open(output_page, 'w') as op:
+            op.write(html_page)
     else:
         print("ERROR: Was not able to collect up-to-date information, examine this log and make changes")
+
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
